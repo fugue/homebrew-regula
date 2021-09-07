cd7179990c483df9839acfb1ff4e52b3ae95b8fb
Fugue <regula@fugue.co>
2021-09-07 16:09:07 -0400

    Brew formula update for regula version v1.3.1

--- Formula/regula.rb
+++ Formula/regula.rb
@@ -5,24 +5,24 @@
 class Regula < Formula
   desc "Evaluate infrastructure as code for security and compliance"
   homepage "https://regula.dev"
-  version "1.3.0"
+  version "1.3.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/fugue/regula/releases/download/v1.3.0/regula_1.3.0_macOS_x86_64.tar.gz"
-    sha256 "acc1be0ba8d386acae5c81ee692ad2543a110c97dcb882cfb6a3359fcebe257b"
+    url "https://github.com/fugue/regula/releases/download/v1.3.1/regula_1.3.1_macOS_x86_64.tar.gz"
+    sha256 "5793bcc9d7254a17175f511b5697db884f1572d93243b959f73c5578aa99edf1"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fugue/regula/releases/download/v1.3.0/regula_1.3.0_macOS_arm64.tar.gz"
-    sha256 "0f0295a6df17917bf63c568f3f162c24782ed957d692afc38629f1f588758594"
+    url "https://github.com/fugue/regula/releases/download/v1.3.1/regula_1.3.1_macOS_arm64.tar.gz"
+    sha256 "bcf7bdbeac7f12b62992ee1f26d583e58023ac59da81ee01ddfbc0f872cf24d8"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fugue/regula/releases/download/v1.3.0/regula_1.3.0_Linux_x86_64.tar.gz"
-    sha256 "5f0aff06003c1d9a6776c81f50a39a5940b62d725e91d9e6b1cc158c229b3bef"
+    url "https://github.com/fugue/regula/releases/download/v1.3.1/regula_1.3.1_Linux_x86_64.tar.gz"
+    sha256 "ebf249f01507297944d71371898281c00116bd3135f5fc542e62601eb4f1ae6b"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/fugue/regula/releases/download/v1.3.0/regula_1.3.0_Linux_arm64.tar.gz"
-    sha256 "d71bb8fb5b243d8d460f54b306f160c21639ea0a101a79dddb3046cf56484ed5"
+    url "https://github.com/fugue/regula/releases/download/v1.3.1/regula_1.3.1_Linux_arm64.tar.gz"
+    sha256 "745a06952af93eb0a51484e93d8c3dfcdb91735dfec6376dec13b3abf574367e"
   end
 
   def install
