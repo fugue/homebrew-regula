5cab571e79d4f5ead6eec040f6bab1e5b93f3898
Fugue <regula@fugue.co>
2021-09-30 09:30:51 -0400

    Brew formula update for regula version v1.5.0

--- Formula/regula.rb
+++ Formula/regula.rb
@@ -5,20 +5,20 @@
 class Regula < Formula
   desc "Evaluate infrastructure as code for security and compliance"
   homepage "https://regula.dev"
-  version "1.4.0"
+  version "1.5.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/fugue/regula/releases/download/v1.4.0/regula_1.4.0_macOS_x86_64.tar.gz"
-    sha256 "22e18fd164b5b82cfac921fdbe5670da3df7bf8493480a142c9213034b8f89e5"
+    url "https://github.com/fugue/regula/releases/download/v1.5.0/regula_1.5.0_macOS_x86_64.tar.gz"
+    sha256 "27f97c526dbe35876dc052c70dc500981ba6842d546948be8a5dadaeed69ad10"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/fugue/regula/releases/download/v1.4.0/regula_1.4.0_Linux_x86_64.tar.gz"
-    sha256 "4ab9c2665e13c769287086a56cf94a47a60322f758697542a68d30783ab8e874"
+    url "https://github.com/fugue/regula/releases/download/v1.5.0/regula_1.5.0_Linux_x86_64.tar.gz"
+    sha256 "565a57f839eb14931f64ff5f0f1268c9eeda71c42f08c88138e420fc385f9d29"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/fugue/regula/releases/download/v1.4.0/regula_1.4.0_Linux_arm64.tar.gz"
-    sha256 "9f0bc2a765ad7b4fe0236ac965a163bc0910f665f81dfb726de0f19d0e02bb4a"
+    url "https://github.com/fugue/regula/releases/download/v1.5.0/regula_1.5.0_Linux_arm64.tar.gz"
+    sha256 "2aa6343e78af3ad5a3f22fe459f5cb34e39b6beb3acf14f0453c45aaab08297a"
   end
 
   def install
